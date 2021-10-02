990f6b250944a417c18eb78b485dd5b2c89de176
RonxldWilson <ronaldwilson00000@gmail.com>
2021-10-02 19:20:15 +0530

    Basic UI Build of App

--- app/src/main/java/com/example/encrytionapp/MainActivity.java
+++ app/src/main/java/com/example/encrytionapp/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     //ON CREATE
     //Creates the Activity UI
+    //add a dropdown box in plact of the textfield (http://www.ahotbrew.com/android-dropdown-spinner-example/)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
