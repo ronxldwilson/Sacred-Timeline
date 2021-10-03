408cb242a4a80ab424ac62366fb6abf75bce4c45
RonxldWilson <ronaldwilson00000@gmail.com>
2021-10-03 18:12:02 +0530

    ID changes

--- app/src/main/java/com/example/encrytionapp/MainActivity.java
+++ app/src/main/java/com/example/encrytionapp/MainActivity.java
@@ -4,12 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-//MAIN ACTIVITY
 public class MainActivity extends AppCompatActivity {
-
-    //ON CREATE
-    //Creates the Activity UI
-    //add a dropdown box in plact of the textfield (http://www.ahotbrew.com/android-dropdown-spinner-example/)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
