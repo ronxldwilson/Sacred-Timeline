290d6dddfaf2754fdfa39a5d10f1e46fb2f68c3c
RonxldWilson <ronaldwilson00000@gmail.com>
2021-10-03 23:17:31 +0530

    new

--- app/src/main/java/com/example/encrytionapp/MainActivity.java
+++ app/src/main/java/com/example/encrytionapp/MainActivity.java
@@ -1,19 +1,16 @@
 package com.example.encrytionapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    Button decryptButton;
-    Button encryptButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
