9b30c1c77466f33b2d0d3684b89a5810e3559dd3
RonxldWilson <ronaldwilson00000@gmail.com>
2021-10-03 22:32:02 +0530

    new Activity

--- app/src/main/java/com/example/encrytionapp/MainActivity.java
+++ app/src/main/java/com/example/encrytionapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.encrytionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.addUser);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
