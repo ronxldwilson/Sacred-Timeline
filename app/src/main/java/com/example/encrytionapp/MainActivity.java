f5a6e77eb57a7c3e1e0a8ab6a22d72a05f94dd44
RonxldWilson <ronaldwilson00000@gmail.com>
2021-10-04 20:11:56 +0530

    Changes

--- app/src/main/java/com/example/encrytionapp/MainActivity.java
+++ app/src/main/java/com/example/encrytionapp/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
